b1f14c15c8914d38e4f8cdc81c4ccd3073e7bdc3
Ulises Medrano <neoodiseo@gmail.com>
2018-10-14 22:14:41 -0500

    #2 add note

--- src/components/NotePage.vue
+++ src/components/NotePage.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="note">
-    <input type="text" v-model="note.title">
+    <input type="text" placeholder="Title of the note" v-model="formNote.title">
+    <textarea rows="5" v-model="formNote.body" placeholder="Body of the note"></textarea>
+    <div><button v-on:click="save">Save</button><button v-on:click="cancel">Cancel</button></div>
   </div>
 </template>
 
@@ -10,11 +12,44 @@ export default {
   props : ['note'],
   data () {
     return {
+      formNote : this.note
     }
   },
+  methods : {
+    save(){
+      this.$emit('saveNote',Object.assign({}, this.formNote));
+      this.formNote = { title : '', body : ''};
+    },
+    cancel(){
+      this.formNote = { title : '', body : ''};
+      this.save();
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+  .note {
+    margin: 20px;
+    padding: 10px;
+    width: 400px;
+    margin-left: auto;
+    margin-right: auto;
+
+    input {
+      padding: 10px;
+      font-size: 18px;
+      width: 350px;
+    }
+    textarea {
+      padding: 10px;
+      font-size: 18px;
+      width: 350px;
+      display: block;
+      margin: 10px;
+      margin-left: auto;
+      margin-right: auto;
+    }
+  }
 </style>
--- src/components/NotesList.vue
+++ src/components/NotesList.vue
@@ -2,7 +2,8 @@
   <div class="notes">
     <h1>Notes</h1>
     <div class="list-options">
-      <note-page v-bind:note="note" />
+      <button v-show="!showForm" v-on:click="toggleForm">Add note</button>
+      <note-page v-show="showForm" v-bind:note="note" v-on:saveNote="save"/>
     </div>
     <ul>
       <li v-for="note in notes">
@@ -21,7 +22,8 @@ export default {
   name: 'NotesList',
   data () {
     return {
-      note : { title : 'test', body : ''},
+      showForm : false,
+      note : { title : '', body : ''},
       notes : [
         { title : 'Step 1', body : 'Create a placeholder for all the notes' },
         { title : 'Step 2', body : 'Create two placeholders for all the notes' },
@@ -31,10 +33,19 @@ export default {
   },
   methods : {
     add(){
-
+      this.note = { title : '', body : ''};
     },
     edit(_note){
       this.note = _note;
+    },
+    save : function (_note) {
+      if(_note.title != '' && _note.body != ''){
+        this.notes.push(_note);
+      }
+      this.toggleForm();
+    },
+    toggleForm : function (){
+      this.showForm = !this.showForm;
     }
   },
   components : {
@@ -45,33 +56,33 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-
-.note {
-  background-color: #D7F0F2;
-  -webkit-box-shadow: 10px 10px 10px 0px rgba(0,0,0,0.5);
-  -moz-box-shadow: 10px 10px 10px 0px rgba(0,0,0,0.5);
-  box-shadow: 10px 10px 10px 0px rgba(0,0,0,0.5);
-
-  .title {
-    background-color: #A2E0E7;
-    padding: 5px;
+  h3 {
+    margin: 40px 0 0;
   }
-  .body {
-    padding: 10px;
+  ul {
+    list-style-type: none;
+    padding: 0;
   }
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+
+  .note {
+    background-color: #D7F0F2;
+    -webkit-box-shadow: 10px 10px 10px 0px rgba(0,0,0,0.5);
+    -moz-box-shadow: 10px 10px 10px 0px rgba(0,0,0,0.5);
+    box-shadow: 10px 10px 10px 0px rgba(0,0,0,0.5);
+
+    .title {
+      background-color: #A2E0E7;
+      padding: 5px;
+    }
+    .body {
+      padding: 10px;
+    }
+  }
 </style>
